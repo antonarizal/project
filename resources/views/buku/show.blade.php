3040040899f11fc029d8e5a8a6a1aebe3d962061
Anton Arizal <antonarizal@gmail.com>
2024-09-26 06:40:08 +0700

    update crud

--- resources/views/buku/show.blade.php
+++ resources/views/buku/show.blade.php
@@ -10,7 +10,7 @@
     <div class="col-md-6">
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Edit Buku</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Informasi Buku</h6>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
